d38c163b84ca39fc5e795f4bfaffde53c71dfba8
Radu Olaret <raduolaret1@gmail.com>
2023-05-10 13:02:39 +0300

    Preliminary Interpretor skeleton

---
+++ Interpreter/Interpreter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Interpreter
+{
+    public class Interpreter
+    {
+        private Dictionary<string, List<IKlppCommand>> _hotkeyScripts;
+        //private CommandFactory _commandFactory;
+
+        /// <summary>
+        /// Interpret the given script and return the hotkeys to be listened for as triggers.
+        /// </summary>
+        /// <param name="script"></param>
+        /// <param name="hotkeys"></param>
+        public Interpreter(in string script, out List<string> hotkeys)
+        {
+            hotkeys = new List<string>();
+            //Step 1: Read the hotkey and send it to the Intermediary.
+            //Step 2: Read the script line by line and construct Command objects (maybe a CommandFactory?).
+            //Step 3: Repeat until end of string.
+
+            Regex hotkeyPattern = new Regex("(.+)::\\n({\\n(?:\\s{4}.+\\n)+})");
+
+            Match hotkey = hotkeyPattern.Match(script);
+
+            while (hotkey.Success)
+            {
+                hotkeys.Append(hotkey.Groups[1].Value);
+                string[] lines = hotkey.Groups[2].Value.Split('\n');
+                List<IKlppCommand> hotkeyCommandList = new List<IKlppCommand>();
+
+                foreach(string line in lines)
+                {
+                    //TODO: CommandFactory?
+                    //IKlppCommand  command = _commandFactory.parse(line);
+                    //hotkeyCommandList.Add(command);
+                }
+
+                _hotkeyScripts.Add(hotkey.Groups[1].Value, hotkeyCommandList);
+            }
+        }
+        /// <summary>
+        /// This method will be called by the Intermediary when it detects that the given hotkey was pressed.
+        /// </summary>
+        /// <param name="hotkey"></param>
+        public void Run(in string hotkey)
+        {
+            List<IKlppCommand> commandList;
+            if(!_hotkeyScripts.TryGetValue(hotkey, out commandList))
+            {
+                //throw new HotkeyNotFoundException;
+            }
+            foreach(IKlppCommand command in commandList)
+            {
+                command.Execute();
+            }
+        }
+    }
+}
