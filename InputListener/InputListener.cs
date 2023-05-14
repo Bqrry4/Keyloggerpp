bd8c8c617d8e26e6e06109a3ac37aff2ceb3b46a
Bqrry <bqrry4@gmail.com>
2023-05-14 19:56:21 +0300

    Added InputListener that listens, still unaware of what is it listening to...

---
+++ InputListener/InputListener.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputListener
+{
+    internal class InputListener
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="nCode"></param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static IntPtr LowLevelInputProc(int nCode, UIntPtr wParam, IntPtr lParam)
+        {
+
+            if (nCode >= 0)
+            {
+                if ((LLInput.KeyEvent)wParam.ToUInt32() == LLInput.KeyEvent.WM_SYSKEYDOWN)
+                {
+                    //Console.WriteLine((Keys)Marshal.ReadInt32(lParam));
+                }
+            }
+
+            return LLInput.CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam);
+        }
+    }
+}
---
+++ InputListener/LLInput.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace InputListener
+{
+    /// <summary>
+    /// Incapsulate the winApi part for low level input
+    /// </summary>
+    internal static class LLInput
+    {
+        public delegate IntPtr HookCallback(int nCode, UIntPtr wParam, IntPtr lParam);
+
+        public enum KeyEvent : int
+        {
+            WM_KEYDOWN = 0x0100,
+            WM_KEYUP = 0x0101,
+            WM_SYSKEYDOWN = 0x0104,
+            WM_SYSKEYUP = 0x0105,
+
+            WM_LBUTTONDOWN = 0x0201,
+            WM_LBUTTONUP = 0x0202,
+            WM_RBUTTONDOWN = 0x0204,
+            WM_RBUTTONUP = 0x0205
+        }
+
+        public enum HookType : int
+        {
+            WH_KEYBOARD_LL = 13,
+            WH_MOUSE_LL = 14
+        }
+
+
+        public static IntPtr SetHook(HookCallback proc, HookType idHook)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx((int)idHook, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetWindowsHookEx(int idHook, HookCallback lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll")]
+        public static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, UIntPtr wParam, IntPtr lParam);
+
+    }
+}
