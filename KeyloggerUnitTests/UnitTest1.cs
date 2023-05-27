bb6430af496f5941eadb5f6af7f104d636d63a67
onofrei.grigore <onofrei.grigore@student.tuiasi.ro>
2023-05-27 15:27:42 +0300

    Listener test

--- KeyloggerUnitTests/UnitTest1.cs
+++ KeyloggerUnitTests/UnitTest1.cs
@@ -4,6 +4,8 @@ using InputListener;
 using Interpreter;
 using Recorder;
 using Logface;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 
 namespace KeyloggerUnitTests
 {
@@ -11,8 +13,46 @@ namespace KeyloggerUnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ListenerTestMethod()
         {
+            //listener to be tested
+            ConcurrentQueue<LLEventData> queue = new ConcurrentQueue<LLEventData>();
+            LLListener listener = new LLListener(queue);
+
+            //interpreter for generating an event
+            ScriptInterpreter interpreter = new ScriptInterpreter();
+            List<string> hots;
+
+            listener.StartListening();
+            interpreter.Parse("Ctrl J::\n{\nSend(\"a\")\n}",out hots);
+            listener.StopListening();
+
+            LLEventData testAction = new LLEventData
+            {
+                eType = 0,
+                kEvent = new KeyEventData
+                {
+                    count = 1,
+                    uChar = "a",
+                    vkCode = (uint)VirtualKeys.A
+                }
+        };
+
+            LLEventData action;
+            if(queue.TryDequeue(out action))
+            {
+                Assert.AreEqual(action, testAction);
+            }
+            else
+            {
+                Assert.Fail("Didn't receive event!");
+            }
+        }
+
+        [TestMethod]
+        public void TestTest()
+        {
+            Assert.IsTrue(true);
         }
     }
 }
