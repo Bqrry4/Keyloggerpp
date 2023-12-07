1471bc95261ce536dfe3a5ae5a23ccbb3f454c6a
onofrei.grigore <onofrei.grigore@student.tuiasi.ro>
2023-12-07 21:13:50 +0200

    AvaloniaEdit writer

---
+++ KeyloggerIDE/Views/AvalonEditorWriter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AvaloniaEdit;
+using Recorder;
+
+namespace KeyloggerIDE.Views
+{
+    internal class AvalonEditorWriter : IWriter
+    {
+        /// <summary>
+        /// TextEditor to which commands will be written
+        /// </summary>
+        private TextEditor Output;
+
+        /// <summary>
+        /// Constructor with output parameter
+        /// </summary>
+        /// <param name="output">TextEditor for commands output</param>
+        public AvalonEditorWriter(TextEditor output)
+        {
+            Output = output;
+        }
+
+        /// <summary>
+        /// Unused in this class
+        /// </summary>
+        public void Close() { }
+
+        /// <summary>
+        /// Write to output
+        /// </summary>
+        /// <param name="text">Text to be written</param>
+        public void Write(string text)
+        {
+            Output.Text += text;
+        }
+    }
+}
